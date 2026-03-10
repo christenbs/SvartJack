b57f9efd1fb00dfd21d522086f5d1c8436e41cbc
ChristenS <christenbstaib@gmail.com>
2026-03-10 18:21:01 +0100

    Improvement to addPlayer and deposit

--- src/main/java/SvartJack/SvartJackController.java
+++ src/main/java/SvartJack/SvartJackController.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
@@ -13,6 +14,14 @@ public class SvartJackController {
     /*Felter som kan manipuleres */
     @FXML
     private TextField addPlayerTextField;
+    @FXML
+    private Button addPlayerButton1;
+    @FXML
+    private Button addPlayerButton2;
+    @FXML
+    private Button depositButton1;
+    @FXML
+    private Button depositButton2;
 
     @FXML
     private TextField depositNameTextField;
@@ -55,9 +64,24 @@ public class SvartJackController {
     public void initialize() {
         labels = new ArrayList<>(Arrays.asList(player1Label, player2Label, player3Label, player4Label, player5Label));
         balances = new ArrayList<>(Arrays.asList(player1Balance, player2Balance, player3Balance, player4Balance, player5Balance));
+
+        addPlayerButton2.setOpacity(0.0);
+        addPlayerTextField.setOpacity(0.0);
+
+        depositButton1.setOpacity(0.0);
+        depositButton2.setOpacity(0.0);
+        depositNameTextField.setOpacity(0.0);
+        depositAmountTextField.setOpacity(0.0);
     }
 
     /*Funkjsonalitet */
+    @FXML
+    public void addPlayer1() {
+        addPlayerButton1.setOpacity(0.0);
+        addPlayerButton2.setOpacity(100.0);
+        addPlayerTextField.setOpacity(100.0);
+    }
+
     @FXML
     public void addPlayer() {
         String name = addPlayerTextField.getText();
@@ -67,11 +91,16 @@ public class SvartJackController {
             addPlayerTextField.setPromptText("Atleast 3 characters");
             addPlayerTextField.clear();
         }
-        if (svartJack.getPlayers().size() > 4) {
+        else if (svartJack.getPlayers().size() > 4) {
             addPlayerTextField.setPromptText("Maksimum 5 players");
             addPlayerTextField.clear();
         }
         else {
+
+            if (svartJack.getPlayers().size() == 0) {
+                depositButton1.setOpacity(100);
+            }
+
             svartJack.add_player(name);
             addPlayerTextField.clear();
             
@@ -81,7 +110,18 @@ public class SvartJackController {
             setBalanceIndex++;
     
             addPlayerTextField.setPromptText("Player name");
+            addPlayerTextField.setOpacity(0.0);
+            addPlayerButton2.setOpacity(0.0);
+            addPlayerButton1.setOpacity(100.0);
         }
+    }
+
+    @FXML
+    public void deposit1() {
+        depositButton1.setOpacity(0.0);
+        depositButton2.setOpacity(100.0);
+        depositNameTextField.setOpacity(100.0);
+        depositAmountTextField.setOpacity(100.0);
     }
 
     @FXML
@@ -89,15 +129,28 @@ public class SvartJackController {
         int amount = Integer.valueOf(depositAmountTextField.getText());
         String name = depositNameTextField.getText();
 
-        svartJack.getPlayers().stream()
-        .forEach(c -> {
-            if (c.getName().equals(name)) {
-                c.deposit(amount);
-                int oldIndex = setBalanceIndex;
-                int setBalanceIndex = svartJack.getPlayers().indexOf(c);
-                balances.get(setBalanceIndex).setText(Integer.toString(svartJack.getPlayers().getLast().getBalance()));
-                setBalanceIndex = oldIndex;
-            }
-        });
+        if (svartJack.getPlayers().stream().noneMatch(c -> c.getName().equals(name))) {
+            depositNameTextField.setPromptText("Can not find: " + name);
+        }
+        else {
+            svartJack.getPlayers().stream()
+            .forEach(c -> {
+                if (c.getName().equals(name)) {
+                    c.deposit(amount);
+                    int oldIndex = setBalanceIndex;
+                    int setBalanceIndex = svartJack.getPlayers().indexOf(c);
+                    balances.get(setBalanceIndex).setText(Integer.toString(svartJack.getPlayers().getLast().getBalance()));
+                    setBalanceIndex = oldIndex;
+    
+                    depositButton1.setOpacity(100.0);
+                    depositButton2.setOpacity(0.0);
+                    depositNameTextField.setOpacity(0.0);
+                    depositAmountTextField.setOpacity(0.0);
+                    depositNameTextField.setPromptText("Player name");
+                }
+            });
+        }
+        depositNameTextField.clear();
+        depositAmountTextField.clear();
     }
 }
